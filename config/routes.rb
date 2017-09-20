46e70ad59f06114e2f76a6803a95f20e38aff075
patricia <patycaballero.1995@gmail.com>
2017-09-20 19:20:24 -0400

    cambios de rutas

--- config/routes.rb
+++ config/routes.rb
@@ -13,5 +13,9 @@ Rails.application.routes.draw do
   resources :registro_puestos
   resources :registro_guardia
   resources :guardia
+
+  get 'regis_guardia/index'
+  get 'regis_guardia/show'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'regis_guardia#index'
 end
