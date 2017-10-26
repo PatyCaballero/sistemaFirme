a027e3b7ecd9d74676a43fad4c86506f9fd30869
patricia <patycaballero.1995@gmail.com>
2017-10-26 20:16:33 -0300

    productos y categorias

--- app/controllers/productos_controller.rb
+++ app/controllers/productos_controller.rb
@@ -69,6 +69,6 @@ class ProductosController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def producto_params
-      params.require(:producto).permit(:nomProducto, :caracteristica)
+      params.require(:producto).permit(:nomProducto, :caracteristica, :categoria_id)
     end
 end
