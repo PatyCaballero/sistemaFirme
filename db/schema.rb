1a53225c1bc3666002a9442fce89a836fad16eef
patricia <patycaballero.1995@gmail.com>
2018-03-04 21:09:32 -0300

    cosas importantes

--- db/schema.rb
+++ db/schema.rb
@@ -10,24 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171005125001) do
+ActiveRecord::Schema.define(version: 20180303221626) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "asignar_puestos", force: :cascade do |t|
-    t.integer "asignarId"
-    t.string "puesto"
-    t.string "direccion"
-    t.string "turno"
-    t.time "horaEntrada"
-    t.time "horaSalida"
+  create_table "calendarios", force: :cascade do |t|
+    t.string "diasTrabajados"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
-  create_table "calendarios", force: :cascade do |t|
-    t.string "diasTrabajados"
+  create_table "categorias", force: :cascade do |t|
+    t.string "nombre"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
@@ -40,6 +35,12 @@ ActiveRecord::Schema.define(version: 20171005125001) do
     t.boolean "estado"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "email"
+    t.string "gerente"
+    t.string "rubro"
+    t.string "sucursales"
+    t.string "descripcion"
+    t.date "fechaInicio"
   end
 
   create_table "control_stock", force: :cascade do |t|
@@ -49,8 +50,41 @@ ActiveRecord::Schema.define(version: 20171005125001) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "guardias", force: :cascade do |t|
+    t.string "nombre"
+    t.string "apellido"
+    t.integer "numDocumento"
+    t.string "direccion"
+    t.string "telefono"
+    t.date "fechaNacimiento"
+    t.string "nacionalidad"
+    t.string "estadoCivil"
+    t.integer "edad"
+    t.string "grupoSanguineo"
+    t.string "estudios"
+    t.string "nombrePadre"
+    t.string "nombreMadre"
+    t.string "direccionAlternativo"
+    t.string "telUrgencia"
+    t.date "fechaIngreso"
+    t.string "sueldoInicial"
+    t.string "observacion"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "imprimir_lista", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "lista_calendarios", force: :cascade do |t|
     t.integer "sueldo"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "listas", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
@@ -66,6 +100,42 @@ ActiveRecord::Schema.define(version: 20171005125001) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "presupuestooos", force: :cascade do |t|
+    t.date "fecha"
+    t.boolean "turno"
+    t.integer "cantHora"
+    t.integer "cantGuardia"
+    t.string "montoMensual"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "turno_id"
+  end
+
+  create_table "producto_puestos", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "puesto_id"
+    t.integer "producto_id"
+    t.integer "cantidadAsignada"
+  end
+
+  create_table "productos", force: :cascade do |t|
+    t.string "nomProducto"
+    t.string "caracteristica"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "categoria_id"
+    t.string "identificador"
+    t.integer "cantidad"
+  end
+
+  create_table "puesto_puestos", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "puesto_id"
+    t.integer "guardia_id"
+  end
+
   create_table "puestos", force: :cascade do |t|
     t.string "direccion"
     t.string "turno"
@@ -74,29 +144,23 @@ ActiveRecord::Schema.define(version: 20171005125001) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "cliente_id"
-    t.integer "regis_guardium_id"
     t.boolean "estado"
-  end
-
-  create_table "regis_guardia", force: :cascade do |t|
-    t.string "nombre"
-    t.string "apellido"
-    t.integer "numDocumento"
-    t.string "direccion"
-    t.integer "telefono"
-    t.string "fechaNacimiento"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.boolean "estado"
+    t.date "fechaInicio"
+    t.date "fechaFin"
+    t.integer "producto_id"
+    t.integer "cantidad"
+    t.integer "cantidadDisponible"
+    t.integer "guardia_id"
   end
 
   create_table "reportes", force: :cascade do |t|
-    t.time "horaEntrada"
-    t.time "horaSalida"
     t.string "estado"
     t.string "novedad"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "puesto_id"
+    t.datetime "horaEntrada"
+    t.datetime "horaSalida"
   end
 
   create_table "stock", force: :cascade do |t|
@@ -105,8 +169,25 @@ ActiveRecord::Schema.define(version: 20171005125001) do
     t.string "descripcion"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "stock"
+    t.integer "producto_id"
   end
 
+  create_table "turnos", force: :cascade do |t|
+    t.string "nombre"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_foreign_key "presupuestooos", "turnos"
+  add_foreign_key "producto_puestos", "productos"
+  add_foreign_key "producto_puestos", "puestos"
+  add_foreign_key "productos", "categorias"
+  add_foreign_key "puesto_puestos", "guardias"
+  add_foreign_key "puesto_puestos", "puestos"
   add_foreign_key "puestos", "clientes"
-  add_foreign_key "puestos", "regis_guardia"
+  add_foreign_key "puestos", "guardias"
+  add_foreign_key "puestos", "productos"
+  add_foreign_key "reportes", "puestos"
+  add_foreign_key "stock", "productos"
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+	Guardia.create ([{nombre: 'Hector', apellido: 'Vieyra', numDocumento: 5140289, direccion: 'Monseñor Wissen 1989', telefono: '0975-179065', fechaNacimiento: '12/06/1989', nacionalidad: 'Paraguayo', estadoCivil: 'Divorsiado', edad: 28, grupoSanguineo: 'A negativo', estudios: 'universitario', nombrePadre: 'Orlando Vieyra', nombreMadre: 'Antonia Acosta', direccionAlternativo: 'Chamorro 1240, Barrio Villa Cristina', telUrgencia: '0985-202020', fechaIngreso: '19/02/2018', sueldoInicial: '1.500.000', observacion: 'falta de documentos probatorios' }, {nombre: 'Juan', apellido: 'Lopez', numDocumento: 5140289, direccion: 'Monseñor Wissen 1989', telefono: '0975-179065', fechaNacimiento: '12/06/1989', nacionalidad: 'Paraguayo', estadoCivil: 'Divorsiado', edad: 28, grupoSanguineo: 'A negativo', estudios: 'universitario', nombrePadre: 'Orlando Vieyra', nombreMadre: 'Antonia Acosta', direccionAlternativo: 'Chamorro 1240, Barrio Villa Cristina', telUrgencia: '0985-202020', fechaIngreso: '19/02/2018', sueldoInicial: '1.500.000', observacion: 'falta de documentos probatorios' }, {nombre: 'Pedro', apellido: 'Benitez', numDocumento: 5140289, direccion: 'Monseñor Wissen 1989', telefono: '0975-179065', fechaNacimiento: '12/06/1989', nacionalidad: 'Paraguayo', estadoCivil: 'Divorsiado', edad: 28, grupoSanguineo: 'A negativo', estudios: 'universitario', nombrePadre: 'Orlando Vieyra', nombreMadre: 'Antonia Acosta', direccionAlternativo: 'Chamorro 1240, Barrio Villa Cristina', telUrgencia: '0985-202020', fechaIngreso: '19/02/2018', sueldoInicial: '1.500.000', observacion: 'falta de documentos probatorios' }])
+  puts 'Se ha creado Guardia'
+	
+  Cliente.create ([{nombre:'Patricia Caballero', num_ruc: '5140289-0', telefono: '098582858', direccion: 'Monseñor Wissen 1989', email: 'paty@gmail.com', gerente: 'Patricia', rubro: 'Banco', sucursales: 2, descripcion: 'Muy buen cliente', fechaInicio: '12/05/2015' }, {nombre: 'Antonia Caballero', num_ruc: '5140289-0', telefono: '098582858', direccion: 'Monseñor Wissen 1989', email: 'paty@gmail.com', gerente: 'Patricia', rubro: 'Banco', sucursales: 2, descripcion: 'Muy buen cliente', fechaInicio: '12/05/2015' }, {nombre: 'Ramon Caballero', num_ruc: '5140289-0', telefono: '098582858', direccion: 'Monseñor Wissen 1989', email: 'paty@gmail.com', gerente: 'Patricia', rubro: 'Banco', sucursales: 2, descripcion: 'Muy buen cliente', fechaInicio: '12/05/2015' }])
+  puts 'Se ha creado clientes'
+
+
+  Categoria.create ([{nombre:'Arma'}, {nombre:'Vestuario'}, {nombre:'Comunicación'}])
+  puts 'Se ha creado Categoria'
+   
+
+  Producto.create ([{nomProducto:'Pistola', caracteristica: 'Automatica Calibre 22', categoria_id: 1, identificador: 'MK035', cantidad: 10, cantidadDisponible: 10 }, {nomProducto:'Chaleco', caracteristica: 'Comun', categoria_id: 2, identificador: 'CH001', cantidad: 10, cantidadDisponible: 10 }, {nomProducto:'Celular', caracteristica: 'Solo llamada', categoria_id: 3, identificador: 'CEL0024', cantidad: 10, cantidadDisponible: 10 }])
+  puts 'Se ha creado Producto'
+
+
+  #Puesto.create ([{direccion:'Tomas Romero Pereira', turno: 'Mañana', hora_entrada: '06:00', hora_salida: '14:00', cliente_id: 1, regis_guardium_id: 1, fechaInicio: '19/02/2018', fechaFin: '19/03/2018', producto_id: 2, cantidad: 1}, {direccion:'25 de Mayo c/ Constitucion', turno: 'Tarde', hora_entrada: '14:00', hora_salida: '21:00', cliente_id: 2, regis_guardium_id: 2, fechaInicio: '19/01/2018', fechaFin: '19/07/2018', producto_id: 1, cantidad: 1}])
+  #puts 'Se ha creado Puesto'
